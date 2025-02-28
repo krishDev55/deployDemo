037f44f0246c9f6bd3821d9fa9ef9e28a8eea045
Actsys 10 <Actsys 10@DESKTOP-Q76TTOQ>
2025-02-28 22:52:39 +0530

    MyApp Chaking

--- src/main/java/com/deployDemo/controller/HomeController.java
+++ src/main/java/com/deployDemo/controller/HomeController.java
@@ -4,13 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/app/v1/")
 public class HomeController {
 
 	@GetMapping("/home")
 		public String homePage() {
-			return "welcome to home page";
+		
+		
+			return "home";
 		}
 	
 	@GetMapping("/login")
